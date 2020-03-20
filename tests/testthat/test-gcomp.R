2df14ffc4b0dc4449724611d71de93e5d97a2fb5
Jessica Grembi <jess.grembi@gmail.com>
2020-03-19 19:22:06 -0700

    Updated testthat tests for gComp and pointEstimate functions

--- tests/testthat/test-gcomp.R
+++ tests/testthat/test-gcomp.R
@@ -1,17 +1,25 @@
-context("test-gcomp")
+context("test-gComp")
 
-test_that("Y and X vars are both provided if no formula is given", {
-  expect_error(gComp(data = cvdd, Y = "cvd_dth", Z = c("BMI", "SEX"), family = "binomial", R = 25))
-  expect_error(pointEstimate(data = cvdd, X = "DIABETES", Z = c("AGE", "SEX"), family = "binomial"))
+testthat::test_that("outcome.type is matched correcly (is not a family like regular glm)", {
+  testthat::expect_error(gComp(data = cvdd, formula = cvd_dth ~ DIABETES + BMI, outcome.type = "binomial", R = 4))
+  testthat::expect_error(gComp(data = cvdd, formula = nhosp ~ DIABETES + AGE + SEX + BMI + CURSMOKE + PREVHYP, outcome.type = "poisson", R = 4))
+  testthat::expect_error(gComp(data = cvdd, Y = "glucoseyear6", X = "DIABETES", Z = c("AGE", "SEX", "BMI", "CURSMOKE", "PREVHYP"), outcome.type = "gaussian", R = 4))
 })
 
 
-test_that("outcome is expected value", {
-  testthat::expect_equal(round(gComp(data = cvdd, formula = cvd_dth ~ DIABETES + BMI, family = "binomial", R = 4)$paramEst[[1]], 2), 3.63)
-  testthat::expect_equal(gComp(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), family = "binomial", R = 4)$R, 4)
+testthat::test_that("Y and X vars are both provided if no formula is given", {
+  testthat::expect_error(gComp(data = cvdd, Y = "cvd_dth", Z = c("BMI", "SEX"), outcome.type = "binary", R = 25))
+  testthat::expect_error(pointEstimate(data = cvdd, X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "binary"))
 })
 
-test_that("class is correct", {
-  testthat::expect_is(gComp(data = cvdd, formula = cvd_dth ~ DIABETES + AGE, family = "binomial", R = 4), "gComp")
+
+testthat::test_that("outcome is expected value", {
+  testthat::expect_equal(round(gComp(data = cvdd, formula = cvd_dth ~ DIABETES + BMI, outcome.type = "binary", R = 4)$paramEst[[1]], 2), 3.63)
+  testthat::expect_equal(gComp(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), outcome.type  = "binary", R = 4)$R, 4)
+
+})
+
+testthat::test_that("class is correct", {
+  testthat::expect_is(gComp(data = cvdd, formula = cvd_dth ~ DIABETES + AGE, outcome.type = "binary", R = 4), "gComp")
 })
 
--- tests/testthat/test-pointestimate.R
+++ tests/testthat/test-pointestimate.R
@@ -1,23 +1,25 @@
-context("test-pointestimate")
+context("test-pointEstimate")
 
-test_that("model specification family is matched correcly", {
-  expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), family = "gaussian"))
-  expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), family = "poisson"))
-  expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), family = "Gamma"))
+testthat::test_that("outcome.type is matched correcly (is not a family like regular glm)", {
+  testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "gaussian"))
+  testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "binomial"))
+  testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "Gamma"))
+  testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "Poisson"))
 })
 
-test_that("Y and X vars are both provided if no formula is given", {
-  expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", Z = c("BMI", "SEX"), family = "binomial"))
-  expect_error(pointEstimate(data = cvdd, X = "DIABETES", Z = c("AGE", "SEX"), family = "binomial"))
-})
-
-
-test_that("X variable is a binary (either a factor with 2 levels, or numeric with only 2 values)", {
-  expect_error(pointEstimate(data = cvdd, formula = CVD ~ BMI + AGE, family = "binomial"))
+testthat::test_that("Y and X vars are both provided if no formula is given", {
+  testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", Z = c("BMI", "SEX"), outcome.type = "binary"))
+  testthat::expect_error(pointEstimate(data = cvdd, X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "binary"))
 })
 
 
-test_that("outcome is expected value", {
-  expect_equal(round(pointEstimate(data = cvdd, formula = CVD ~ SEX + BMI + AGE, family = "binomial")$diff,3), -0.315)
-  expect_equal(round(pointEstimate(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), family = "binomial")$ratio, 3), 1.259)
+testthat::test_that("X variable is a binary (either a factor with 2 levels, or numeric with only 2 values)", {
+  testthat::expect_error(pointEstimate(data = cvdd, formula = CVD ~ BMI + AGE, outcome.type = "binary"))
+})
+
+
+testthat::test_that("outcome is expected value", {
+  testthat::expect_equal(round(pointEstimate(data = cvdd, formula = CVD ~ SEX + BMI + AGE, outcome.type = "binary")$diff,3), -0.315)
+  testthat::expect_equal(round(pointEstimate(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), outcome.type = "binary")$ratio, 3), 1.259)
+  testthat::expect_equal(pointEstimate(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), outcome.type  = "binary")$family, "binomial")
 })
