873a0cda3e2d223f767545c715d9dfaa24ccbefb
Jessica Grembi <jess.grembi@gmail.com>
2020-03-27 09:04:42 -0700

    Added test for Z = character error

--- tests/testthat/test-pointestimate.R
+++ tests/testthat/test-pointestimate.R
@@ -5,6 +5,7 @@ testthat::test_that("outcome.type is matched correcly (is not a family like regu
   testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "binomial"))
   testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "Gamma"))
   testthat::expect_error(pointEstimate(data = cvdd, Y = "cvd_dth", X = "DIABETES", Z = c("AGE", "SEX"), outcome.type = "Poisson"))
+  testthat::expect_error(pointEstimate(data = cvdd %>% dplyr::mutate(AGE = as.character(AGE)), formula = "DEATH ~ DIABETES + SEX + AGE", outcome.type = "binary"))
 })
 
 testthat::test_that("Y and X vars are both provided if no formula is given", {
@@ -22,5 +23,5 @@ testthat::test_that("X variable is a binary (either a factor with 2 levels, or n
 testthat::test_that("outcome is expected value", {
   testthat::expect_equal(round(pointEstimate(data = cvdd, formula = CVD ~ SEX + BMI + AGE, outcome.type = "binary")$parameter.estimates[[1,1]],3), -0.145)
   testthat::expect_equal(round(pointEstimate(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), outcome.type = "binary")$parameter.estimates[[2,1]], 3), 1.176)
-  testthat::expect_equal(pointEstimate(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), outcome.type  = "binary")$formula, "DEATH ~ DIABETES + SEX + AGE")
+  testthat::expect_equal(pointEstimate(data = cvdd, Y = "DEATH", X = "DIABETES", Z = c("SEX", "AGE"), outcome.type  = "binary")$formula, formula(DEATH ~ DIABETES + SEX + AGE))
 })
