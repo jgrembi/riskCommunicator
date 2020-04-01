1ab7186a3b0cbc23dc3a73736ad79103b12ec683
Jessica Grembi <jess.grembi@gmail.com>
2020-04-01 09:57:06 -0700

    Update \code{}

--- R/print.gComp.R
+++ R/print.gComp.R
@@ -2,7 +2,7 @@
 #'
 #' @description Print results from bootstrap computations of the g-computation
 #'
-#' @param x (Required) An object of class `gComp`.
+#' @param x (Required) An object of class \code{gComp}.
 #' @param ... (Optional) Further arguments passed to or from other methods.
 #' 
 #' @return The resulting point estimate and 95% confidence intervals of the difference and ratio.
