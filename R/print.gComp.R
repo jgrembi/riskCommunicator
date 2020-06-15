e5efff9425a97e301c2aed148f82e54044f3b0ef
Jessica Grembi <jess.grembi@gmail.com>
2020-06-15 15:16:59 -0700

    Reduced to R = 20

--- R/print.gComp.R
+++ R/print.gComp.R
@@ -19,7 +19,7 @@
 #' data(cvdd)
 #' set.seed(4832)
 #' diabetes.result <- gComp(data = cvdd, Y = "cvd_dth", X = "bmicat",
-#' Z = c("AGE", "SEX", "DIABETES", "CURSMOKE", "PREVHYP"), outcome.type = "binary", R = 50)
+#' Z = c("AGE", "SEX", "DIABETES", "CURSMOKE", "PREVHYP"), outcome.type = "binary", R = 20)
 #' print(diabetes.result)
 #'
 #'
