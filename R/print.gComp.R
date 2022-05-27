9e0de7f8140d39cc445cc9a80becbc623196d82e
JGrembi <jess.grembi@gmail.com>
2022-05-26 21:59:31 -0700

    Moved summary.gComp back to separate file from print.gComp

--- R/print.gComp.R
+++ R/print.gComp.R
@@ -6,14 +6,12 @@
 #' @param x (Required) An object of class \code{gComp} as produced by \code{gComp()}.
 #' @param ... (Optional) Further arguments passed to or from other methods.
 #'
-#' @return For \code{print} returns the formula and resulting point estimate and 95% 
-#' confidence intervals of the  difference and ratio. 
-#' For \code{summary} resturns the formula, family and link function, contrasts, 
-#' parameter estimates with 95% CIs, and a summary of the underlying glm used to 
-#' make predictions.
+#' @return Returns the formula and resulting point estimate and 95% 
+#' confidence intervals of the difference and ratio. 
+#' 
 #'   
 #' @export
-#' @method print gComp summary gComp
+#' @method print gComp 
 #'
 #' @examples
 #' ## Obtain the risk difference and risk ratio for cardiovascular disease or 
@@ -25,10 +23,9 @@
 #' diabetes.result <- gComp(data = cvdd, Y = "cvd_dth", X = "bmicat",
 #' Z = c("AGE", "SEX", "DIABETES", "CURSMOKE", "PREVHYP"), outcome.type = "binary", R = 20)
 #' print(diabetes.result)
-#' summary(diabetes.result)
 #'
 #'
-#' @keywords print.gComp summary.gComp
+#' @keywords print.gComp 
 
 
 
@@ -40,36 +37,4 @@ print.gComp <- function(x, ...) {
   cat("Parameter estimates:", "\n")
   print(format(x$summary))
   
-}
-
-summary.gComp <- function(object, ...) {
-  if(!is(object, "gComp")) stop("Object supplied is not of class 'gComp'")
-  
-  res <- list(formula = object$formula, 
-              family = object$family$family, 
-              link = object$family$link,
-              contrast = object$contrast,
-              summary = object$summary, 
-              underlying_glm = object$glm.result)
-  
-  class(res) <- "summary.gComp"
-  return(res)
-}
-
-print.summary.gComp <- function(x, ...) {
-  cat("Formula:", "\n")
-  cat(format(x$formula, width = 60), "\n") 
-  cat("\n")
-  cat("Family:", x$family, "\n")
-  cat("Link function:", x$link, "\n")
-  cat("\n")
-  cat("Contrast:", x$contrast, "\n")
-  cat("\n")
-  cat("Parameter estimates:", "\n")
-  print(format(x$summary))
-  cat("\n")
-  cat("Underlying glm:")
-  print(x$underlying_glm)
-  # NextMethod("print")
-  invisible(x)
 }
